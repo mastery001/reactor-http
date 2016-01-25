81a722c2e880bc9d9ae1f2a73424551e6622e255
zouziwen <zouziwen@letv.com>
2016-01-25 13:22:25 +0800

    delete HttpInvoker

--- src/main/java/org/http/filter/FailedReportFilter.java
+++ src/main/java/org/http/filter/FailedReportFilter.java
@@ -6,12 +6,12 @@ import org.http.chain.HttpSession;
 
 /**
  * 错误上报过滤器
+ * 
  * @author zouziwen
  *
- * 2016年1月25日 上午11:42:35
+ *         2016年1月25日 上午11:42:35
  */
 public abstract class FailedReportFilter extends HttpFilterAdapter {
-
 
 	public FailedReportFilter() {
 	}
@@ -29,6 +29,14 @@ public abstract class FailedReportFilter extends HttpFilterAdapter {
 		nextFilter.requestFailed(session, responseMessage);
 	}
 
+	/**
+	 * 具体的上报实现
+	 * 
+	 * @param url	concrete request's url
+	 * @param message
+	 *            message there are two possible ，one is {@link Throwable }，
+	 *            other is {@link HttpResponseMessage} 2016年1月25日 下午12:52:11
+	 */
 	protected abstract void report(String url, Object message);
-	
+
 }
